da38e5c9dca819cfc280b0d35557b7c73ee04b79
AdarshGrewal <adarshgrewal@gmail.com>
2026-06-23 15:12:37 +0530

    power: supply: mca: Import charger_partition header from bsp-zorn-v-oss
    Change-Id: I1257c4c768d9210b06ef49cffd1af63b82dc3cbd

---
+++ drivers/power/supply/mca/include/mca/shared_memory/charger_partition_class.h
@@ -0,0 +1,69 @@
+#ifndef _MCA_SHARED_MEMORY_CHARGER_PARTITION_CLASS_H_
+#define _MCA_SHARED_MEMORY_CHARGER_PARTITION_CLASS_H_
+
+#include <linux/types.h>
+
+#define CHARGER_PARTITION_MAGIC 0x20240725
+
+typedef enum {
+	CHARGER_PARTITION_HOST_LK,
+	CHARGER_PARTITION_HOST_UEFI,
+	CHARGER_PARTITION_HOST_ABL,
+	CHARGER_PARTITION_HOST_ADSP,
+	CHARGER_PARTITION_HOST_KERNEL,
+	CHARGER_PARTITION_HOST_HAL,
+	CHARGER_PARTITION_HOST_FRAMEWORK,
+	CHARGER_PARTITION_HOST_APPLICATION,
+	CHARGER_PARTITION_HOST_LAST,
+	CHARGER_PARTITION_HOST_INVALID,
+} charger_partition_host_type;
+
+typedef enum {
+	CHARGER_PARTITION_HEADER,
+	CHARGER_PARTITION_INFO_1,
+	CHARGER_PARTITION_INFO_2,
+	CHARGER_PARTITION_INFO_3,
+	CHARGER_PARTITION_INFO_4,
+	CHARGER_PARTITION_INFO_5,
+	CHARGER_PARTITION_INFO_6,
+	CHARGER_PARTITION_INFO_7,
+	CHARGER_PARTITION_INFO_8,
+	CHARGER_PARTITION_INFO_9,
+	CHARGER_PARTITION_INFO_10,
+	CHARGER_PARTITION_INFO_LAST,
+	CHARGER_PARTITION_INFO_INVALID,
+} charger_partition_info_type;
+
+typedef struct {
+	u32 magic;
+	u32 version;
+	u32 initialized;
+	u32 avaliable;
+	u32 reserved;
+} charger_partition_header;
+
+typedef struct {
+	u32 power_off_mode;
+	u32 zero_speed_mode;
+	u32 test;
+	u32 reserved;
+} charger_partition_info_1;
+
+typedef struct {
+	u32 eu_mode;
+	u32 test;
+	u32 reserved;
+} charger_partition_info_2;
+
+int charger_partition_alloc(u8 charger_partition_host_type,
+			    u8 charger_partition_info_type, uint32_t size);
+int charger_partition_dealloc(u8 charger_partition_host_type,
+			      u8 charger_partition_info_type, uint32_t size);
+void *charger_partition_read(u8 charger_partition_host_type,
+			     u8 charger_partition_info_type, uint32_t size);
+int charger_partition_write(u8 charger_partition_host_type,
+			    u8 charger_partition_info_type, void *buf,
+			    uint32_t size);
+int charger_partition_get_eu_model(bool *is_eu_model);
+
+#endif /* _MCA_SHARED_MEMORY_CHARGER_PARTITION_CLASS_H_ */
