327941d7608b9955301a725544cdb1bd8814f192
AdarshGrewal <adarshgrewal@gmail.com>
2026-06-23 15:03:41 +0530

    power: supply: mca: Setup headers for new mca_platform classes
    Change-Id: I111cc2f2772e37fbc579510188da4d39d558f944

---
+++ drivers/power/supply/mca/include/mca/platform/platform_bc12_class.h
@@ -0,0 +1,23 @@
+#ifndef _MCA_PLATFORM_PLATFORM_BC12_CLASS_H_
+#define _MCA_PLATFORM_PLATFORM_BC12_CLASS_H_
+
+#include <linux/types.h>
+
+/* BC1.2 detector role (order = index into the per-role ops array). */
+enum bc12_role {
+	BC12_MAIN_ROLE = 0,
+	BC12_AUX_ROLE,
+	BC12_MAX_ROLE,
+};
+
+struct platform_bc12_class_ops {
+	int (*bc12_det_en)(int en, void *data);
+	int (*get_charge_type)(int *value, void *data);
+	int (*bc12_det_done)(void *data, bool *value);
+};
+
+int platform_bc12_class_ops_register(unsigned int role,
+				     struct platform_bc12_class_ops *ops,
+				     void *data);
+
+#endif /* _MCA_PLATFORM_PLATFORM_BC12_CLASS_H_ */
