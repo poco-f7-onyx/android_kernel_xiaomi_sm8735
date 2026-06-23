99171e712a1cdd088a61f63d830472101ae9975f
AdarshGrewal <adarshgrewal@gmail.com>
2026-06-23 15:29:07 +0530

    power: supply: mca: Setup headers for mca_protocol*
    Change-Id: I300051e4eb2142b30c3c9d15e9638caf87f25232

--- drivers/power/supply/mca/include/mca/protocol/protocol_class.h
+++ drivers/power/supply/mca/include/mca/protocol/protocol_class.h
@@ -2,6 +2,17 @@
 #define _MCA_PROTOCOL_PROTOCOL_CLASS_H_
 
 #include <linux/types.h>
+#include <linux/platform_device.h>
+#include <linux/of.h>
+#include <linux/mod_devicetable.h>
+
+enum adapter_protocol_type {
+	ADAPTER_PROTOCOL_BC12 = 0,
+	ADAPTER_PROTOCOL_PD,
+	ADAPTER_PROTOCOL_PPS,
+	ADAPTER_PROTOCOL_QC,
+	ADAPTER_PROTOCOL_MAX,
+};
 
 enum mca_adapter_type {
 	ADAPTER_NONE = 0,
@@ -14,5 +25,94 @@ enum mca_adapter_type {
 	ADAPTER_XIAOMI_QC3,
 	ADAPTER_AUTH_FAILED,
 };
+
+enum xm_charger_type {
+	XM_CHARGER_TYPE_UNKNOW = 0,
+	XM_CHARGER_TYPE_SDP,
+	XM_CHARGER_TYPE_CDP,
+	XM_CHARGER_TYPE_DCP,
+	XM_CHARGER_TYPE_FLOAT,
+	XM_CHARGER_TYPE_OCP,
+	XM_CHARGER_TYPE_ACA,
+	XM_CHARGER_TYPE_TYPEC,
+	XM_CHARGER_TYPE_HVDCP2,
+	XM_CHARGER_TYPE_HVDCP3,
+	XM_CHARGER_TYPE_HVDCP3_B,
+	XM_CHARGER_TYPE_HVDCP3P5,
+	XM_CHARGER_TYPE_PD,
+	XM_CHARGER_TYPE_PD_VERIFY,
+	XM_CHARGER_TYPE_PPS,
+};
+
+enum typec_port_index {
+	TYPEC_PORT_0 = 0,
+	TYPEC_PORT_1,
+	TYPEC_PORT_MAX,
+};
+
+enum typec_attach_mode {
+	TYPEC_UNATTACH = 0,
+	TYPEC_SNK_MODE,
+	TYPEC_SRC_MODE,
+	TYPEC_AUDIO_ACCESS_MODE,
+	TYPEC_DEBUG_ACCESS_MODE,
+};
+
+#define ADAPTER_CAP_MAX_NR	10
+
+struct adapter_power_cap_entry {
+	int max_voltage;
+	int min_voltage;
+	int max_current;
+	int max_power;
+};
+
+struct adapter_power_cap_info {
+	int nums;
+	struct adapter_power_cap_entry cap[ADAPTER_CAP_MAX_NR];
+};
+
+
+struct adapter_vendor_info {
+	unsigned int vid;
+	unsigned int pid;
+	unsigned char hw_ver;
+	unsigned char fw_ver;
+};
+
+struct adapter_power_curve {
+	int nums;
+	int volt[ADAPTER_CAP_MAX_NR];
+	int curr[ADAPTER_CAP_MAX_NR];
+};
+
+struct adapter_protocol_class_ops {
+	int (*set_adapter_verified)(void *data, int verified);
+	int (*get_adapter_verified)(void *data, int *verified);
+	int (*get_adapter_max_power)(void *data, unsigned int *max_power);
+	int (*adapter_det_en)(void *data, int en);
+	int (*get_adapter_type)(void *data, int *type);
+	int (*adapter_bc12_det_done)(void *data, bool *value);
+	int (*get_adapter_pwr_cap)(void *data, struct adapter_power_cap_info *cap);
+	int (*set_adapter_volt_and_curr)(void *data, int volt, int curr);
+	int (*get_adapter_volt_and_curr)(void *data, int *volt, int *curr);
+	int (*get_adapter_pps_ptf)(void *data, int *pps_ptf);
+	int (*get_adapter_info)(void *data, struct adapter_vendor_info *info);
+	int (*get_adapter_power_curve)(void *data, struct adapter_power_curve *pwr_curve);
+};
+
+int protocol_class_register_ops(unsigned int protocol, struct adapter_protocol_class_ops *ops, void *data);
+int protocol_class_set_adapter_verified(unsigned int protocol, int verified);
+int protocol_class_get_adapter_verified(unsigned int protocol, int *verified);
+int protocol_class_get_adapter_max_power(unsigned int protocol, unsigned int *max_power);
+int protocol_class_det_adapter_type(unsigned int protocol, int en);
+int protocol_class_get_adapter_type(unsigned int protocol, unsigned int *value);
+int protocol_class_get_bc12_adapter_detect_done(unsigned int protocol, bool *value);
+int protocol_class_get_adapter_power_cap(unsigned int protocol, struct adapter_power_cap_info *cap);
+int protocol_class_set_adapter_volt_and_curr(unsigned int protocol, int volt, int curr);
+int protocol_class_get_adapter_volt_and_curr(unsigned int protocol, int *volt, int *curr);
+int protocol_class_get_adapter_pps_ptf(unsigned int protocol, int *pps_ptf);
+int protocol_class_get_adapter_info(unsigned int protocol, struct adapter_vendor_info *info);
+int protocol_class_get_adapter_power_curve(unsigned int protocol, struct adapter_power_curve *pwr_curve);
 
 #endif /* _MCA_PROTOCOL_PROTOCOL_CLASS_H_ */
